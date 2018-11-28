2a07fd630cc4760cffebdda89cf6de18ee6cd2ce
heinzjp <heinzjp@rose-hulman.edu>
2018-11-28 09:14:44 -0500

    Done

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -89,7 +89,7 @@ def turtles(t, color):
 
 
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #   COMMIT-and-PUSH your work (after changing this TO-DO to DONE).
 #
 #   As a reminder, here is how you should do so:
